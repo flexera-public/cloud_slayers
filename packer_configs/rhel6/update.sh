85113a1e99009ec9423d38a0bbaaddd4bc53870e
Bill <bill.rich@gmail.com>
2016-08-26 15:15:30 -0700

    Fix bash mistake

--- packer_configs/rhel6/update.sh
+++ packer_configs/rhel6/update.sh
@@ -5,7 +5,7 @@ if `which yum &> /dev/null`; then
   sudo rm -fr /var/cache/yum/*
   sudo yum clean all
   sudo yum update -y
-elsif `which apt-get &> /dev/null`; then
+elif `which apt-get &> /dev/null`; then
   echo "Updating debian based system"
   sudo apt-get update
   sudo apt-get dist-upgrade -y
