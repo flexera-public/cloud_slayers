1675b4d56e7755711b066a210dbaf3a6fce5fcf6
Bill <bill.rich@gmail.com>
2016-08-26 15:04:25 -0700

    Make scripts more generic

--- packer_configs/rhel6/cleanup.sh
+++ packer_configs/rhel6/cleanup.sh
@@ -1,2 +1,12 @@
 #!/bin/bash -ex
-sudo /usr/sbin/waagent -force -deprovision+user
+if `which waagent &> /dev/null`; then
+  sudo /usr/sbin/waagent -force -deprovision+user
+else
+  sudo rm -rf /var/lib/cloud/ /tmp/*
+  sudo rm -f /var/log/cloud-init* /etc/udev/rules.d/70-persistent-net.rules
+  sudo find /root -name authorized_keys -type f -exec rm -f {} \;
+  sudo find /home -name authorized_keys -type f -exec rm -f {} \;
+  sudo find /var/log -type f -exec cp /dev/null {} \;
+  history -c
+  sync
+fi
--- packer_configs/rhel6/update.sh
+++ packer_configs/rhel6/update.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-sudo rm -fr /var/cache/yum/*
-sudo yum clean all
-sudo yum update -y
+if `which yum &> /dev/null`; then
+  echo "Updating yum based system"
+  sudo rm -fr /var/cache/yum/*
+  sudo yum clean all
+  sudo yum update -y
+elsif `which apt-get &> /dev/null`; then
+  echo "Updating debian based system"
+  sudo apt-get update
+  sudo apt-get dist-upgrade -y
+else
+  echo "Uknown package manager. No updates will run"
+fi
